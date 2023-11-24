9b5e5ed966b3dd324ede99f4af87f232c1ec7747
I-Zuniga <126074680+I-Zuniga@users.noreply.github.com>
2023-11-24 16:48:59 +0100

    utils created

---
+++ lib/utils.py
@@ -0,0 +1,9 @@
+#Utils from event camera attitude determination 
+
+# set diferent type of features combinations
+
+class features: 
+    def __init__(self, type):
+        self.type = type 
+        pass
+        
