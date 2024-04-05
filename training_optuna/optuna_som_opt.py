a47009d579ca815786f59eb9c5562842072d2137
I-Zuniga <126074680+I-Zuniga@users.noreply.github.com>
2024-04-05 12:48:53 +0200

    Update optuna_som_opt.py

--- training_optuna/optuna_som_opt.py
+++ training_optuna/optuna_som_opt.py
@@ -31,49 +31,53 @@ def objective(trial):
     # Number of layers and units per layer for actor
 
     # SOM 1 
-    # mesh_size_1 = trial.suggest_int('mesh_size_1', 60,90)
-    sigma_1 = trial.suggest_float('sigma_1', 1, 5)
-    learning_rate_1 = trial.suggest_float('learning_rate_1', 0.2, 2)
-    # neighborhood_function_1 = trial.suggest_categorical('neighborhood_function_1', ['gaussian', 'triangle'])
-    # topology_1 = trial.suggest_categorical('topology_1', ['hexagonal', 'rectangular'])
-    # activation_distance_1 = trial.suggest_categorical('activation_distance_1', ['euclidean', 'manhattan'])
+    mesh_size1_x = trial.suggest_int('mesh_size1_x', 60,90)
+    mesh_size1_y = trial.suggest_int('mesh_size1_y', 60,90)
+    sigma_1 = trial.suggest_float('sigma_1', 2, 5)
+    learning_rate_1 = trial.suggest_float('learning_rate_1', 0.6, 2)
+    neighborhood_function_1 = trial.suggest_categorical('neighborhood_function_1', ['gaussian', 'triangle'])
+    topology_1 = trial.suggest_categorical('topology_1', ['hexagonal', 'rectangular'])
+    activation_distance_1 = trial.suggest_categorical('activation_distance_1', ['euclidean', 'manhattan'])
     feature_type_1 = trial.suggest_categorical('feature_type_1', ['log_polar', 'permutation', 'permutation_log', 'permutation_log_polar', 'permutation_angle', 'permutation_angle_dist'])
 
-    # mesh_size_2 = trial.suggest_int('mesh_size_2', 60,90)
-    sigma_2 = trial.suggest_float('sigma_2', 1, 5)
-    learning_rate_2 = trial.suggest_float('learning_rate_2', 0.2, 2)
-    # neighborhood_function_2 = trial.suggest_categorical('neighborhood_function_2', ['gaussian', 'triangle'])
-    # topology_2 = trial.suggest_categorical('topology_2', ['hexagonal', 'rectangular'])
-    # activation_distance_2 = trial.suggest_categorical('activation_distance_2', ['euclidean', 'manhattan'])
-    feature_type_2 = trial.suggest_categorical('feature_type_2', ['log_polar', 'permutation', 'permutation_log', 'permutation_log_polar', 'permutation_angle', 'permutation_angle_dist'])
+    mesh_size2_x = trial.suggest_int('mesh_size2_x', 60,90)
+    mesh_size2_y = trial.suggest_int('mesh_size2_y', 60,90)
+    sigma_2 = trial.suggest_float('sigma_2', 2, 5)
+    learning_rate_2 = trial.suggest_float('learning_rate_2', 0.6, 2)
+    neighborhood_function_2 = trial.suggest_categorical('neighborhood_function_2', ['gaussian', 'triangle'])
+    topology_2 = trial.suggest_categorical('topology_2', ['hexagonal', 'rectangular'])
+    activation_distance_2 = trial.suggest_categorical('activation_distance_2', ['euclidean', 'manhattan'])
+    feature_type_2 = trial.suggest_categorical('feature_type_2', ['log_polar', 'permutation', 'permutation_log', 'permutation_log_polar','permutation_angle', 'permutation_angle_dist'])
 
 
 
     hyperparameters_1 = {
-        'mesh_size_1': 50,
+        'mesh_size_x': mesh_size1_x,
+        'mesh_size_y': mesh_size1_y,
         'sigma': sigma_1,
         'learning_rate': learning_rate_1,
-        'neighborhood_function': 'triangle',
-        'topology': 'rectangular',
-        'activation_distance': 'euclidean',
+        'neighborhood_function': neighborhood_function_1,
+        'topology': topology_1,
+        'activation_distance': activation_distance_1,
         'n_of_neighbors' : 4,
-        'epochs' : 500000,
+        'epochs' : 100000,
         'feature_type': feature_type_1,
     }
 
     hyperparameters_2 = {
-        'mesh_size_2': 50,
+        'mesh_size_x': mesh_size2_x,
+        'mesh_size_y': mesh_size2_y,
         'sigma': sigma_2,
         'learning_rate': learning_rate_2,
-        'neighborhood_function': 'triangle',
-        'topology': 'rectangular',
-        'activation_distance': 'euclidean',
+        'neighborhood_function': neighborhood_function_2,
+        'topology': topology_2,
+        'activation_distance': activation_distance_2,
         'n_of_neighbors' : 4,
-        'epochs' : 500000,
+        'epochs' : 100000,
         'feature_type': feature_type_2,
     }
 
-    accuracy = features_search(hyperparameters_1, hyperparameters_2, trial)
+    accuracy = train(hyperparameters_1, hyperparameters_2, trial)
    
     
     return accuracy
@@ -84,10 +88,10 @@ def objective(trial):
 print("Study statistics: ")
 
 study = optuna.create_study(
-    direction='maximize', # single objective
-    # directions=['maximize', 'maximize','maximize'], # multi-objective
+    # direction='maximize', # single objective
+    directions=['maximize', 'maximize','maximize'], # multi-objective
     storage="sqlite:///db.sqlite3",
-    study_name="features_search",
+    study_name="Multi Objective SOM Optimization",
     load_if_exists=True,
     pruner=optuna.pruners.ThresholdPruner(lower=0.1),
     )
