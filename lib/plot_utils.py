46f2d8d54e16c9fb5eb4b92f1a491d5f0eef7ee2
I-Zuniga <126074680+I-Zuniga@users.noreply.github.com>
2024-04-12 20:15:48 +0200

    Update plot_utils.py

--- lib/plot_utils.py
+++ lib/plot_utils.py
@@ -1,3 +1,4 @@
+from matplotlib import axis
 from metavision_core.event_io import EventsIterator
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,7 +12,12 @@ def plot_image(img):
     fig, ax = plt.subplots()
     ax.imshow(img, cmap='gray')
     fig.set_size_inches(18.5, 10.5)
+    #delete axis
+    ax.axis('off')
+
     plt.show()
+
+    
 
     # Pause for 0.01 seconds
     plt.pause(0.01)
@@ -54,9 +60,9 @@ def plot_cluster(img, clusters, cluster_size, size = [10, 7]):
         #         ax.plot([cluster[0][1] - cluster_size, cluster[0][1] - cluster_size, cluster[0][1] + cluster_size, cluster[0][1] + cluster_size, cluster[0][1] - cluster_size],
         #                 [cluster[0][0] - cluster_size, cluster[0][0] + cluster_size, cluster[0][0] + cluster_size, cluster[0][0] - cluster_size, cluster[0][0] - cluster_size], 'g', linestyle='dotted')
                 
-        # Plot the cluster number as text in the top left corner of the cluster
-                for i, cluster in enumerate(clusters):
-                        ax.text(cluster[2][1] - cluster_size, cluster[2][0] - cluster_size -5, i+1, color='r')
+        # # Plot the cluster number as text in the top left corner of the cluster
+        #         for i, cluster in enumerate(clusters):
+        #                 ax.text(cluster[2][1] - cluster_size, cluster[2][0] - cluster_size -5, i+1, color='r')
 
         # Plot a red cross on the center of mass of the clusters
                 ax.plot([cluster[0][1] - 2, cluster[0][1] + 2],
@@ -80,10 +86,16 @@ def plot_cluster(img, clusters, cluster_size, size = [10, 7]):
                 # Plot a red cross on the initial position of the clusters
                 ax.plot([cluster[1] - 2, cluster[1] + 2],
                         [cluster[0] - 2, cluster[0] + 2], 'r')
+                ax.plot([cluster[1] - 2, cluster[1] + 2],
+                        [cluster[0] + 2, cluster[0] - 2], 'r')
                 # Plot the cluster number as text in the top left corner of the cluster
-                for i, cluster in enumerate(clusters):
-                        ax.text(cluster[1] - cluster_size, cluster[0] - cluster_size -5, i+1, color='r')
+                # for i, cluster in enumerate(clusters):
+                #         ax.text(cluster[1] - cluster_size, cluster[0] - cluster_size -5, i+1, color='r')
     fig.set_size_inches(size[0], size[1])
+
+#     Crop the image to the origina size
+    ax.set_xlim(0, img.shape[1])
+    ax.set_ylim(img.shape[0], 0)
 
     # Axis off
     ax.axis('off')
@@ -124,6 +136,8 @@ def plot_cluster_with_id(img, clusters, ids, size = [10, 7], center = False):
                 # Plot a red cross on the initial position of the clusters
                 ax.plot([cluster[1] - 2, cluster[1] + 2],
                         [cluster[0] - 2, cluster[0] + 2], 'r')
+                ax.plot([cluster[1] - 2, cluster[1] + 2],
+                        [cluster[0] + 2, cluster[0] - 2], 'r')
                 # Plot the cluster number as text in the top left corner of the cluster
                 for i, cluster in enumerate(clusters):
                         ax.text(cluster[1] - 5, cluster[0]  -5, ids[i], color='r')
@@ -142,6 +156,70 @@ def plot_cluster_with_id(img, clusters, ids, size = [10, 7], center = False):
     # Show the plot
     plt.show()
 
+def plot_cluster_cv(frame, clusters_list,confirmed_stars_ids,original_confirmed_ids,frame_position = None, size = None, show_confirmed_ids = False):
+        '''
+        Plot the clusters on the image. 
+
+        size : list 
+            Size of the image.
+
+        Returns
+        -------
+        None.
+        '''
+        img_rgb = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+
+        cluster_size = 15
+        clusters =  clusters_list
+
+
+        for i, cluster in enumerate(clusters):
+                
+            # Plot a square around the initial clusters position with size cluster_size
+            cv2.rectangle(img_rgb, (cluster[1] - cluster_size, cluster[0] - cluster_size),
+                        (cluster[1] + cluster_size, cluster[0] + cluster_size), (0, 0, 255), 1)
+            # Plot a red cross on the initial position of the clusters
+            cv2.drawMarker(img_rgb, (cluster[1], cluster[0]), (0, 0, 255), markerType=cv2.MARKER_CROSS, markerSize=5)
+
+            # Plot the cluster number as text in the top left corner of the cluster
+
+            if show_confirmed_ids and  confirmed_stars_ids is not None:
+
+                # Full confirmed stars  stars (light green)
+                if confirmed_stars_ids[i] is not None:
+
+                    if confirmed_stars_ids[i]  not in original_confirmed_ids: # Full confirmed stars (light green)
+                        cv2.putText(img_rgb, str(confirmed_stars_ids[i]), (cluster[1] - cluster_size, cluster[0] - cluster_size - 5),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 1)
+                        # Update the square and the cross to green if the star is confirmed
+                        cv2.rectangle(img_rgb, (cluster[1] - cluster_size, cluster[0] - cluster_size),
+                            (cluster[1] + cluster_size, cluster[0] + cluster_size), (255, 255, 0), 1)
+                        cv2.drawMarker(img_rgb, (cluster[1], cluster[0]), (255, 255, 0), markerType=cv2.MARKER_CROSS, markerSize=5)
+                        
+                    else: # Original confirmed stars (green)
+                        cv2.putText(img_rgb, str(confirmed_stars_ids[i]), (cluster[1] - cluster_size, cluster[0] - cluster_size - 5),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+                        # Update the square and the cross to green if the star is confirmed
+                        cv2.rectangle(img_rgb, (cluster[1] - cluster_size, cluster[0] - cluster_size),
+                            (cluster[1] + cluster_size, cluster[0] + cluster_size), (0, 255, 0), 1)
+                        cv2.drawMarker(img_rgb, (cluster[1], cluster[0]), (0, 255, 0), markerType=cv2.MARKER_CROSS, markerSize=5)
+                        
+            else:
+                for i, cluster in enumerate(clusters):
+                    cv2.putText(img_rgb, str(i+1), (cluster[1] - cluster_size, cluster[0] - cluster_size - 5),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+        
+
+        if frame_position is not None:
+            cv2.putText(img_rgb, 'Frame position: ' + str(frame_position), (10, 20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+        # # # Resize image
+        if size is not None:
+            img_rgb_resized = cv2.resize(img_rgb, (size[0], size[1]))
+            return img_rgb_resized
+        else:       
+            return img_rgb
+        
 def plot_cluster_comparation(img, clusters_batchs, cluster_size):
     '''
     Plot the clusters on the image. 
